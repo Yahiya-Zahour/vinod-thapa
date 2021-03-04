f77ad0035bf84fceb7ab335448fe1700eccf105a
Yahya-Zahour <76777699+Yahya-Zahour@users.noreply.github.com>
2021-03-04 15:42:19 +0530

    javascrpit work

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+// import './index.css';
 
+
+const name ="thapa world";
+const img1 ="https://picsum.photos/340/300";
+const img2 ="https://picsum.photos/330/300";
+const img3 ="https://picsum.photos/320/300";
+const img4 ="https://picsum.photos/310/300";
+const links ='http://localhost:3000/';
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <>
+<h1 className="heading">zahour{name}</h1>
+<img src={img1} />
+<img src={img2} />
+<img src={img3} />
+<a href={links} target="cr7">
+<img src={img4} />
+</a>
+</>, 
+  document.getElementById("root"));
