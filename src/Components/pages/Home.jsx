d7b93d17ce395f8543171873fb803adb02a1fac6
Yahya-Zahour <76777699+Yahya-Zahour@users.noreply.github.com>
2021-10-05 14:43:17 +0530

    practice

--- src/Components/pages/Home.jsx
+++ src/Components/pages/Home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
            <h1>hyyyy</h1>
     <img src="Assets/photo1.jpg" alt="" width="400px" height="400px"/>
     <button>ok</button>
+    <button>ok</button>
 
         </div>
     );
