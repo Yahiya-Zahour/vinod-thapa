6f33408bc4148654f5dac9c9bfc52bbaabed865b
Yahya-Zahour <76777699+Yahya-Zahour@users.noreply.github.com>
2021-10-07 14:51:32 +0530

    practice

--- src/Components/Nav/Navbar.jsx
+++ src/Components/Nav/Navbar.jsx
@@ -13,6 +13,11 @@ const Navbar = () => {
       </nav>
  <button>ok</button>
   
+<button>ok</button>
+    <p>Lorem odit incidunt eos vitae. Vel quae reprehenderit eligendi aperiam blanditiis obcaecati!</p>
+    <button>ok</button>
+    <p>Lorem odit incidunt eos vitae. Vel quae reprehenderit eligendi aperiam blanditiis obcaecati!</p>
+  
         </div>
     );
 };
