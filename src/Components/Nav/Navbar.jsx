e06c02fd222cb951e7ed8455bc347c706d630173
Yahya-Zahour <76777699+Yahya-Zahour@users.noreply.github.com>
2021-10-07 14:51:14 +0530

    practice

--- src/Components/Nav/Navbar.jsx
+++ src/Components/Nav/Navbar.jsx
@@ -11,12 +11,8 @@ const Navbar = () => {
         <NavLink to="/Service">SERVICE</NavLink>
     
       </nav>
- 
-   
-<button>ok</button>
-    <p>Lorem odit incidunt eos vitae. Vel quae reprehenderit eligendi aperiam blanditiis obcaecati!</p>
-    <button>ok</button>
-    <p>Lorem odit incidunt eos vitae. Vel quae reprehenderit eligendi aperiam blanditiis obcaecati!</p>
+ <button>ok</button>
+  
         </div>
     );
 };
