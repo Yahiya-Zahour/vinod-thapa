a26b116da1f9125464e75c65ac115da9c127cc05
Yahya-Zahour <76777699+Yahiya-Zahour@users.noreply.github.com>
2021-10-25 11:40:57 +0530

    practice

--- src/Components/Home/Landing.jsx
+++ src/Components/Home/Landing.jsx
@@ -26,7 +26,7 @@ const Landing = () => {
           </Route>
         </Switch>
       </Router>
-      <button>login</button>
+    
       <button>login</button>
       <button>login</button>
       <button>login</button>
